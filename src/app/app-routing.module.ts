551784f4205675420a09195982fbdd0abad98877
shrouk mamdoh <eng.shrouk.mamdoh@gmail.com>
2022-06-17 12:16:38 +0200

    shrouk mamdoh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'sidebar', component:SidebarComponent},
+  {path: 'products', component:ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
